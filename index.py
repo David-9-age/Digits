8ab9153ee7382bbe1fcbff32c2cd70e6561bd212
David-9-age <davidumea20@gmail.com>
2025-11-30 13:38:55 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+# Program to count the total number of digits in a number
+
+# Ask the user to enter a number
+num = input("Enter a number: ")
+
+# Count the digits
+digit_count = len(num)
+
+# Display the result
+print("The total number of digits in the number is:", digit_count)
